28103005e9c4ff5255edddb92b00d606e4dd2273
Leonardo Santos <92178094+LeoSantosp2@users.noreply.github.com>
2025-01-10 23:57:02 -0300

    Criado component Details Books Modal

---
+++ src/components/details-book-modal/index.tsx
@@ -0,0 +1,32 @@
+import { IoCloseOutline } from 'react-icons/io5';
+
+import { DetailsBooksModalProps } from '../../types/details-books-modal-props';
+
+import { Modal, Container } from './styled';
+
+export default function DetailsBookModal(props: DetailsBooksModalProps) {
+  return (
+    <Modal>
+      <Container>
+        <header>
+          <h1>Node React Test</h1>
+
+          <IoCloseOutline size={24} onClick={props.onClose} />
+        </header>
+
+        <main>
+          <ul>
+            <li>Título: {props.book.title}</li>
+            <li>Autor: {props.book.author}</li>
+            <li>Código da Cópia: {props.book.copyCode}</li>
+            <li>ISBN: {props.book.isbn}</li>
+          </ul>
+        </main>
+
+        <footer>
+          <button>Alugar</button>
+        </footer>
+      </Container>
+    </Modal>
+  );
+}
---
+++ src/components/details-book-modal/styled.ts
@@ -0,0 +1,74 @@
+import styled from 'styled-components';
+
+export const Modal = styled.div`
+  width: 100%;
+  height: 100vh;
+  position: absolute;
+  top: 0;
+  transition: all 0.3s ease;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.3);
+`;
+
+export const Container = styled.div`
+  width: 40rem;
+  height: 30rem;
+  border-radius: 1rem;
+  box-shadow: 0.3rem 0.3rem 1rem rgba(0, 0, 0, 0.1);
+  background-color: ${({ theme }) => theme.colors.white};
+
+  header {
+    width: 100%;
+    height: 10%;
+    padding: 0 1rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-size: ${({ theme }) => theme.font.sizes.subText};
+  }
+
+  header svg {
+    cursor: pointer;
+  }
+
+  main {
+    width: 100%;
+    height: 75%;
+    padding: 0 1rem;
+    display: flex;
+    align-items: center;
+  }
+
+  main ul li {
+    margin: 1rem 0;
+    list-style: none;
+    font-size: ${({ theme }) => theme.font.sizes.text};
+  }
+
+  footer {
+    width: 100%;
+    height: 15%;
+    padding: 0 1rem;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+  }
+
+  footer button {
+    width: 8rem;
+    height: 2.7rem;
+    border-radius: 0.5rem;
+    border: 0;
+    transition: all 0.3s ease;
+    cursor: pointer;
+    font-size: ${({ theme }) => theme.font.sizes.text};
+    background-color: ${({ theme }) => theme.colors.black};
+    color: ${({ theme }) => theme.colors.white};
+
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+`;
