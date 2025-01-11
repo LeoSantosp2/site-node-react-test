ee6703e561e067202b8982c7e5dcdbba3740916d
Leonardo Santos <92178094+LeoSantosp2@users.noreply.github.com>
2025-01-10 23:56:29 -0300

    Criado arquivo env.ts contendo algumas variáveis

---
+++ src/config/env.ts
@@ -0,0 +1,16 @@
+export default {
+  MAX_ITEMS: 5,
+  MAX_LEFT: (5 - 1) / 2, // (MAX_ITEMS - 1) / 2
+  LIMIT: 5,
+  DEFAULT_BOOK_VALUE: {
+    copy_code: '',
+    status: '',
+    book: {
+      id: '',
+      title: '',
+      author: '',
+      copyCode: '',
+      isbn: '',
+    },
+  },
+};
