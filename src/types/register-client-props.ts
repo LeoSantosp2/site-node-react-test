c75424111572bf36f1e7d5a9d3ca1ec80d0a177f
Leonardo Santos <92178094+LeoSantosp2@users.noreply.github.com>
2025-01-30 22:16:27 -0300

    Criado tipos para a função que cadastra clientes

---
+++ src/types/register-client-props.ts
@@ -0,0 +1,12 @@
+export type RegisterClientProps = {
+  name: string;
+  cpf: string;
+  dateOfBirth: string;
+  streetAddress: string;
+  numberAddress: string;
+  complementAddress: string;
+  neighborhoodAddress: string;
+  cityAddress: string;
+  stateAddress: string;
+  zipcodeAddress: string;
+};
