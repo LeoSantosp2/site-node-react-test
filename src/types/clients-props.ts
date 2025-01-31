a8a88dd46e041f5890f161f08d307f09b2e92366
Leonardo Santos <92178094+LeoSantosp2@users.noreply.github.com>
2025-01-30 22:20:15 -0300

    Criado tipos para cliente

---
+++ src/types/clients-props.ts
@@ -0,0 +1,15 @@
+export type ClientsProps = {
+  id: string;
+  name: string;
+  cpf: string;
+  date_of_birth: string;
+  street_address: string;
+  number_address: string;
+  complement_address: string;
+  neighborhood_address: string;
+  city_address: string;
+  state_address: string;
+  zipcode_address: string;
+  created_at: string;
+  updated_at: string;
+};
