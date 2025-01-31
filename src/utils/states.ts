7423cfdebeaf8bb921778bb509b833add26b69b3
Leonardo Santos <92178094+LeoSantosp2@users.noreply.github.com>
2025-01-30 22:14:26 -0300

    Criado util com uma lista dos estados do Brasil

---
+++ src/utils/states.ts
@@ -0,0 +1,165 @@
+import { StateProps } from '../types/state-props';
+
+export const states: StateProps[] = [
+  {
+    id: 1,
+    value: 'Acre',
+    uf: 'AC',
+  },
+
+  {
+    id: 2,
+    value: 'Alagoas',
+    uf: 'AL',
+  },
+
+  {
+    id: 3,
+    value: 'Amapá',
+    uf: 'AP',
+  },
+
+  {
+    id: 4,
+    value: 'Amazonas',
+    uf: 'AM',
+  },
+
+  {
+    id: 5,
+    value: 'Bahia',
+    uf: 'BA',
+  },
+
+  {
+    id: 6,
+    value: 'Ceará',
+    uf: 'CE',
+  },
+
+  {
+    id: 7,
+    value: 'Distrito Federal',
+    uf: 'DF',
+  },
+
+  {
+    id: 8,
+    value: 'Espírito Santo',
+    uf: 'ES',
+  },
+
+  {
+    id: 9,
+    value: 'Goiás',
+    uf: 'GO',
+  },
+
+  {
+    id: 10,
+    value: 'Maranhão',
+    uf: 'MA',
+  },
+
+  {
+    id: 11,
+    value: 'Mato Grosso',
+    uf: 'MT',
+  },
+
+  {
+    id: 12,
+    value: 'Mato Grosso do Sul',
+    uf: 'MS',
+  },
+
+  {
+    id: 13,
+    value: 'Minas Gerais',
+    uf: 'MG',
+  },
+
+  {
+    id: 14,
+    value: 'Pará',
+    uf: 'PA',
+  },
+
+  {
+    id: 15,
+    value: 'Paraíba',
+    uf: 'PB',
+  },
+
+  {
+    id: 16,
+    value: 'Paraná',
+    uf: 'PR',
+  },
+
+  {
+    id: 17,
+    value: 'Pernambuco',
+    uf: 'PE',
+  },
+
+  {
+    id: 18,
+    value: 'Piauí',
+    uf: 'PI',
+  },
+
+  {
+    id: 19,
+    value: 'Rio de Janeiro',
+    uf: 'RJ',
+  },
+
+  {
+    id: 20,
+    value: 'Rio Grande do Norte',
+    uf: 'RN',
+  },
+
+  {
+    id: 21,
+    value: 'Rio Grande do Sul',
+    uf: 'RS',
+  },
+
+  {
+    id: 22,
+    value: 'Rondônia',
+    uf: 'RO',
+  },
+
+  {
+    id: 23,
+    value: 'Roraima',
+    uf: 'RR',
+  },
+
+  {
+    id: 24,
+    value: 'Santa Catarina',
+    uf: 'SC',
+  },
+
+  {
+    id: 25,
+    value: 'São Paulo',
+    uf: 'SP',
+  },
+
+  {
+    id: 26,
+    value: 'Sergipe',
+    uf: 'SE',
+  },
+
+  {
+    id: 27,
+    value: 'Tocantins',
+    uf: 'TO',
+  },
+];
