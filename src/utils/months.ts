aef0a0ea37766058f9015731352070ec6e3534a5
Leonardo Santos <92178094+LeoSantosp2@users.noreply.github.com>
2025-03-08 17:29:22 -0300

    Criado util que tem todos os meses do ano

---
+++ src/utils/months.ts
@@ -0,0 +1,73 @@
+export const months = [
+  {
+    id: 1,
+    value: 'Janeiro',
+    text: 'Janeiro',
+  },
+
+  {
+    id: 2,
+    value: 'Fevereiro',
+    text: 'Fevereiro',
+  },
+
+  {
+    id: 3,
+    value: 'Março',
+    text: 'Março',
+  },
+
+  {
+    id: 4,
+    value: 'Abril',
+    text: 'Abril',
+  },
+
+  {
+    id: 5,
+    value: 'Maio',
+    text: 'Maio',
+  },
+
+  {
+    id: 6,
+    value: 'Junho',
+    text: 'Junho',
+  },
+
+  {
+    id: 7,
+    value: 'Julho',
+    text: 'Julho',
+  },
+
+  {
+    id: 8,
+    value: 'Agosto',
+    text: 'Agosto',
+  },
+
+  {
+    id: 9,
+    value: 'Setembro',
+    text: 'Setembro',
+  },
+
+  {
+    id: 10,
+    value: 'Outubro',
+    text: 'Outubro',
+  },
+
+  {
+    id: 11,
+    value: 'Novembro',
+    text: 'Novembro',
+  },
+
+  {
+    id: 12,
+    value: 'Dezembro',
+    text: 'Dezembro',
+  },
+];
